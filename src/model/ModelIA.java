12b4e308f7b72914e3da3bd1c3b15bc7c36c9894
sergiodpg <sergiodpgcode.@gmail.com>
2023-12-11 17:37:00 +0100

    Añadimos el fichero binario para la serialización y el cuerpo del controller listo

--- src/model/ModelIA.java
+++ src/model/ModelIA.java
@@ -18,7 +18,7 @@ import java.util.List;
  *
  * @author Usuario
  */
-public class ModelIA implements ILLM, IRepository {
+public class ModelIA {
     
     /**
      * 
@@ -32,10 +32,12 @@ public class ModelIA implements ILLM, IRepository {
     private IRepository repository;
     // Tipo de ILLM seleccionado por el usuario
     private ILLM illm;
+    File ficheroEstadoSerializado;
     
     public ModelIA (IRepository repository, ILLM illm) {
         this.repository = repository;
         this.illm = illm;
+        ficheroEstadoSerializado = Paths.get(System.getProperty("user.home"), "Desktop", "jLLM.bin").toFile();
         conversations = new ArrayList<>();
         
     }
@@ -47,9 +49,125 @@ public class ModelIA implements ILLM, IRepository {
      *
      * @return
      */
+    
     public boolean exportConversations() {
-        return repository.exportarAlum nos(alumnos);
+        return repository.exportConversation(conversations);
     }
+    
+    public boolean importConversations() {
+       
+        ArrayList<Conversation> conversacionesImportadas = repository.importConversations();
+        
+        
+        
+        
+        
+        
+    }
+    
+    /**
+     * 
+     * Función de carga del estado previo de la aplicación
+     * 
+     * @return
+     */
+    
+    public boolean cargarEstadoAplicación() {
+
+        if (ficheroEstadoSerializado.exists() && ficheroEstadoSerializado.isFile()) {
+            ObjectInputStream ois = null;
+            try {
+                ois = new ObjectInputStream(new FileInputStream(ficheroEstadoSerializado));
+                this.conversations = (ArrayList<Conversation>) ois.readObject();
+            } catch (IOException | ClassNotFoundException ex) {
+                // Dejamos el error para la depuración, por el canal err.
+                System.err.println("Error durante la deserialización: " + ex.getMessage());
+                return false;
+            } finally {
+                if (ois != null) {
+                    try {
+                        ois.close();
+                    } catch (IOException ex) {
+                        // Dejamos el error para la depuración, por el canal err.
+                        System.err.println("Error durante la deserialización: " + ex.getMessage());
+                        return false;
+                    }
+                }
+            }
+            return true;
+        } else {
+            return false;
+        }
+
+    }
+    
+    /**
+     * 
+     * Función de guardado del estado previo de la aplicación
+     * 
+     * @return
+     */
+
+    public boolean guardarEstadoAplicación() {
+
+        ObjectOutputStream oos = null;
+        try {
+            oos = new ObjectOutputStream(new FileOutputStream(ficheroEstadoSerializado));
+            oos.writeObject(conversations);
+            return true;
+        } catch (IOException ex) {
+            // Dejamos el error para la depuración, por el canal err.
+            System.err.println("Error durante la serialización: " + ex.getMessage());
+            return false;
+        } finally {
+            if (oos != null) {
+                try {
+                    oos.close();
+                } catch (IOException ex) {
+                    // Dejamos el error para la depuración, por el canal err.
+                    System.err.println("Error al cerrar el flujo: " + ex.getMessage());
+                    return false;
+                }
+            }
+        }
+
+    }
+    
+    /**
+     * 
+     * GETTERS Y SETTERS
+     * 
+     * @return
+     */
+    
+    public ArrayList<Conversation> getConversations() {
+        return conversations;
+    }
+
+    public void setConversations(ArrayList<Conversation> conversations) {
+        this.conversations = conversations;
+    }
+
+    public IRepository getRepository() {
+        return repository;
+    }
+
+    public void setRepository(IRepository repository) {
+        this.repository = repository;
+    }
+
+    public ILLM getIllm() {
+        return illm;
+    }
+
+    public void setIllm(ILLM illm) {
+        this.illm = illm;
+    }
+    
+    
+    
+    
+    
     
     
     
